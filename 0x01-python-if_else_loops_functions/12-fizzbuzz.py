4db208d795489e445a2cd4c613ecdefcb3609bf1
Michael <lordkay07@gmail.com>
2024-08-10 06:05:44 +0100

    correcting the expected output of the function

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for n in range(1, 101):
-        if n % 3 == 0:
-            print("Fizz", end="")
+        if n % 15 == 0:
+            print("FizzBuzz", end="")
+        
         elif n % 5 == 0:
             print("Buzz", end="")
-        elif n % 15 == 0:
-            print("FizzBuzz", end="")
+        elif n % 3 == 0:
+            print("Fizz", end="")
         else:
             print(n, end="")
 
         print(" ", end="")
+
+fizzbuzz()
